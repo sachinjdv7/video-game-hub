d71716c06f5ee20df241ea2e02f62244873176e7
sachinjadhav <sachinjdv7@gmail.com>
2024-07-04 00:10:33 +0530

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,11 +7,15 @@ import GameCardContainer from "./GameCardContainer.tsx";
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
 
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={10}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={3}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
