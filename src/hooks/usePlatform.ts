a0335cd739de2e3e1944cb61a141900b5a4bd6bb
sachinjadhav <sachinjdv7@gmail.com>
2024-07-07 23:27:11 +0530

    Refactoring - fetch games with react query

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
 
