8003580938e95ecfe85bfafc935de1cceb10e762
sachinjadhav <sachinjdv7@gmail.com>
2024-07-07 22:14:41 +0530

    fetch genres using react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "./theme.ts";
 import App from "./App.tsx";
 import "./index.css";
+
+const queryClinet = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClinet}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
